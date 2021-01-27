78f45e21988e99b8affe9b0b7bd004af7f345103
jenick <jenick_lee@163.com>
2021-01-27 20:19:01 +0800

    0.5.4

--- app/admin/controller/Ultraman.php
+++ app/admin/controller/Ultraman.php
@@ -5,12 +5,14 @@
  * Date: 2021/1/7
  * Time: 5:56 下午
  */
+
 namespace app\admin\controller;
 
 use app\common\lib\Response;
 use think\App;
 use app\common\service\Ultraman as UltramanService;
 use think\Validate;
+
 class Ultraman extends Base
 {
     public function __construct(App $app)
@@ -40,10 +42,11 @@ class Ultraman extends Base
         $this->obj->setPUserId($param['p_user_id']);
         $this->obj->setAims($param['aims']);
         $this->obj->setDepositBase($param['deposit_base']);
-        $this->obj->setStartTime($param['start_time']?? date('Y-01-01 00:00:00'));
-        $this->obj->setEndTime($param['end_time']?? date('Y-12-31 23:59:59'));
+        $this->obj->setStartTime($param['start_time'] ?? date('Y-01-01 00:00:00'));
+        $this->obj->setEndTime($param['end_time'] ?? date('Y-12-31 23:59:59'));
         try {
-            $this->obj->addUltraman();
+            $res = $this->obj->addUltraman();
+            $this->saveSysLog("管理员[{$this->adminUserInfo['nickname']}]，创建奥特曼[id：{$res}，当前基数为：{$param['deposit_base']}，目标为：{$param['aims']}]");
             return Response::success();
         } catch (\Exception $e) {
             return Response::error(config('code.error'), $e->getMessage());
@@ -88,10 +91,16 @@ class Ultraman extends Base
 
         $this->obj->setUby($this->adminUserId);
         $this->obj->setId($param['id']);
-        $this->obj->setAims($param['aims']??0);
-        $this->obj->setDepositBase($param['deposit_base']??0);
+        $this->obj->setAims($param['aims'] ?? 0);
+        $this->obj->setDepositBase($param['deposit_base'] ?? 0);
         try {
-            $this->obj->editUltraman($param['type'], $param['calculation']);
+            $res = $this->obj->editUltraman($param['type'], $param['calculation']);
+            $str = "当前基数调整为￥{$res['deposit_base']}";
+            if ($param['type'] === 2) {
+                $str = "目标调整为￥{$res['aims']}";
+            }
+
+            $this->saveSysLog("管理员[{$this->adminUserId['nickname']}]，将[id：{$param['id']}}，username：{$res['username']}]的奥特曼的{$str}");
             return Response::success();
         } catch (\Exception $e) {
             return Response::error(config('code.error'), $e->getMessage());

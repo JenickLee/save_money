b514e74d48d1736fc40685cdd59f635410eca438
jenick <jenick_lee@163.com>
2021-02-14 17:16:26 +0800

    v0.11.0

--- app/common/service/UltramanLog.php
+++ app/common/service/UltramanLog.php
@@ -39,26 +39,25 @@ class UltramanLog extends UltramanLogBean
         $this->model->setWhereArr($where);
         $this->model->setField("FROM_UNIXTIME( UNIX_TIMESTAMP( log.create_time ), '%Y-%m-%d' ) date, log.deposit_base value");
         $res = $this->model->getUserDepositBaseDataAnalysis();
-        $newArr = [];
-        for ($i = 0; $i < count($res); $i++) {
-            array_push($newArr, $res[$i]);
-            $startDate =  date('Y-m-d', strtotime($res[$i]['date']) + 86400);
-            $endDate = null;
-            if (!isset($res[$i + 1])) {
-                $endDate = date('Y-m-d');
-            } else if ($startDate != $res[$i + 1]['date']) {
-                $endDate = date('Y-m-d', strtotime($res[$i + 1]['date']) - 86400);
-            }
-
-            if(isset($endDate)){
-                $value = $res[$i]['value'];
-                $dateArr = Date::getDateByInterval($startDate, $endDate);
-                foreach ($dateArr as $vo) {
-                    array_push($newArr, ['date' => $vo, 'value' => $value]);
-                }
-            }
-
-        }
-        return $newArr;
+//        $newArr = [];
+//        for ($i = 0; $i < count($res); $i++) {
+//            array_push($newArr, $res[$i]);
+//            $startDate =  date('Y-m-d', strtotime($res[$i]['date']) + 86400);
+//            $endDate = null;
+//            if (!isset($res[$i + 1])) {
+//                $endDate = date('Y-m-d');
+//            } else if ($startDate != $res[$i + 1]['date']) {
+//                $endDate = date('Y-m-d', strtotime($res[$i + 1]['date']) - 86400);
+//            }
+//
+//            if(isset($endDate)){
+//                $value = $res[$i]['value'];
+//                $dateArr = Date::getDateByInterval($startDate, $endDate);
+//                foreach ($dateArr as $vo) {
+//                    array_push($newArr, ['date' => $vo, 'value' => $value]);
+//                }
+//            }
+//        }
+        return $res;
     }
 }
